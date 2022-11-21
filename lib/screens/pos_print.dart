3d96d9bf1e9253ab7ae3432a58d6eaca5510f6f2
Timesemd <sktimesmed@gmail.com>
2022-11-21 18:47:27 +0530

    working base

--- lib/screens/pos_print.dart
+++ lib/screens/pos_print.dart
@@ -1,10 +1,21 @@
 
+import 'dart:typed_data';
+
 import 'package:dotted_border/dotted_border.dart';
 import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:syncfusion_flutter_barcodes/barcodes.dart';
+import 'package:widgets_to_image/widgets_to_image.dart';
+
+
+import 'dart:io';
+
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
 
 import '../main.dart';
 import '../utlity/blue_print.dart';
@@ -22,6 +33,9 @@ class PrintingWidget extends StatefulWidget {
 
 class _PrintingWidgetState extends State<PrintingWidget> {
   List<ScanResult>? scanResult;
+  WidgetsToImageController controller = WidgetsToImageController();
+  Uint8List? bytes;
+  late File imgFile;
 
   @override
   void initState() {
@@ -38,21 +52,32 @@ class _PrintingWidgetState extends State<PrintingWidget> {
     });
     flutterBlue.stopScan();
   }
+  getSetImage() async{
+    final directory =
+        (await getApplicationDocumentsDirectory()).path;
+    imgFile = File('$directory/photo.png',);
+    Uint8List? pngBytes = await controller.capture();
+    await imgFile.writeAsBytes(pngBytes!);
+    return imgFile.path;
+  }
 
   void printWithDevice(BluetoothDevice device) async {
     await device.connect();
     final gen = Generator(PaperSize.mm58, await CapabilityProfile.load());
     final printer = BluePrint();
     print("PPPPPPPPPPPPPPPPPPPPPPP ${printer.toString()}");
+    //printer.add(gen.hr());
+    printer.add(gen.image(getSetImage()));
 
-    printer.add(gen.text(widget.currenttoken.toString(),styles: PosStyles(height: PosTextSize.size1)));
-    //printer.add(gen.qrcode('${widget.currenttoken} ${now.toString()}',),);
-    var currenttokendatenow = '${widget.currenttoken} ${now.toString()}'.split(',');
-    printer.add(gen.barcode(Barcode.code39([currenttokendatenow])));
-    //printer.add(gen.barcode(Barcode.code39(widget.currenttoken.tolist())));
 
-    printer.add(gen.text('${widget.currenttoken} ${now.toString()}',),);
-    printer.add(gen.feed(1));
+    // printer.add(gen.text(widget.currenttoken.toString(),styles: PosStyles(height: PosTextSize.size1)));
+    // printer.add(gen.qrcode('${widget.currenttoken} ${now.toString()}',),);
+    // var currenttokendatenow = '${widget.currenttoken} ${now.toString()}'.split(',');
+    // printer.add(gen.barcode(Barcode.code39([currenttokendatenow])));
+    // printer.add(gen.barcode(Barcode.code39(widget.currenttoken.tolist())));
+    //printer.add(gen.text('${widget.currenttoken} ${now.toString()}',),);
+    //printer.add(gen.feed(1));
+    printer.add(gen.cut());
     print("PPPPPPPPPPPPPPPPPPPPPPP ${printer.toString()}");
     await printer.printData(device);
 
@@ -71,28 +96,31 @@ class _PrintingWidgetState extends State<PrintingWidget> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  height: 300,
-                  width: 300,
-                  child: DottedBorder(
-                    color: Colors.black,
-                    strokeWidth: 1,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          widget.currenttoken,
-                          style: TextStyle(fontSize: 100),
-                        ),
-                        Container(
-                          height: 80,
-                          child: SfBarcodeGenerator(
-                            value: "${widget.currenttoken} ${now.toString()}",
-                            showValue: true,
+                WidgetsToImage(
+                  controller: controller,
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    child: DottedBorder(
+                      color: Colors.black,
+                      strokeWidth: 1,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            widget.currenttoken,
+                            style: TextStyle(fontSize: 100),
                           ),
-                        ),
-                      ],
+                          Container(
+                            height: 80,
+                            child: SfBarcodeGenerator(
+                              value: "${widget.currenttoken} ${now.toString()}",
+                              showValue: true,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
