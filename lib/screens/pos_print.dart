1541699090aa75b4b4fd648adcea97493e1fba29
Timesemd <sktimesmed@gmail.com>
2023-02-28 11:37:58 +0530

    added progress when printing

--- lib/screens/pos_print.dart
+++ lib/screens/pos_print.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 
 import 'package:dotted_border/dotted_border.dart';
 import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -122,10 +123,10 @@ class _PrintingWidgetState extends State<PrintingWidget> {
     //       styles:  PosStyles(align: PosAlign.center,bold: true,)
     //   ),
     // );
-    printer.add(
-      gen.qrcode('${widget.currenttoken} ${now.toString()}',
-          size: QRSize.Size8, align: PosAlign.center),
-    );
+    // printer.add(
+    //   gen.qrcode('${widget.currenttoken} ${now.toString()}',
+    //       size: QRSize.Size8, align: PosAlign.center),
+    // );
     // print( '${widget.currenttoken} ${now.toString()}');
     // List<String> currenttokendatenow = '${widget.currenttoken.toString()}'.split('');
     // print( 'CCCCCCCCCCCCCCCCCCCCC$currenttokendatenow');
@@ -199,146 +200,171 @@ class _PrintingWidgetState extends State<PrintingWidget> {
   Widget build(BuildContext context) {
     //showAlertDialog(context);
     return Scaffold(
+      backgroundColor: Colors.white,
       key: _scaffoldKey,
       body: SafeArea(
-        child: Stack(
-          children: [
-            // InAppWebView(
-            //     initialUrlRequest: URLRequest(url: Uri.parse("http://signalr.timesmed.com/Login/KaveriLogin")),
-            //
-            //     onLoadStart: (cntrl, url) => onLoadStart(cntrl),
-            //     initialOptions: InAppWebViewGroupOptions(
-            //         android: AndroidInAppWebViewOptions(
-            //           useHybridComposition: true,
-            //           allowContentAccess: true,
-            //         )),
-            //     androidOnPermissionRequest: (InAppWebViewController controller, String origin, List<String> resources) async {
-            //       return PermissionRequestResponse(resources: resources, action: PermissionRequestResponseAction.GRANT);
-            //     }
-            //
-            // ),
-            // FutureBuilder(builder: (context,snap){
-            //   return _launchURL(context);
-            // }),
-            // WV.WebView(
-            //   javascriptMode: WV.JavascriptMode.unrestricted,
-            //   initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
-            //   allowsInlineMediaPlayback: true,
-            //
-            //   initialUrl: 'http://signalr.timesmed.com/Login/KaveriLogin',
-            //   onWebViewCreated: (controller) {
-            //     _controller = controller;
-            //   },
-            //
-            //   // onPageFinished: (_) async {
-            //   //
-            //   //   final String username = "Dashboarduser";
-            //   //   final String password = "121212";
-            //   //
-            //   //   _controller.runJavascript('''
-            //   //                document.getElementById("usrname").value = '$username';
-            //   //               ''');
-            //   //   _controller.runJavascript('''
-            //   //   document.getElementById("password").value ='$password';
-            //   //                ''');
-            //   //
-            //   //   await Future.delayed(Duration(seconds: 1));
-            //   //   _controller.runJavascript(
-            //   //       "document.getElementsByClassName('btn').logClick.click();");
-            //   // },
-            //
-            // ),
-            Container(
-              color: Colors.white,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+        child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    child: Padding(
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.height / 20),
+                      child: DottedBorder(
+                        color: Colors.black,
+                        strokeWidth: 1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              widget.currenttoken,
+                              style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.height / 5),
+                            ),
+                            Container(
+                              height: 80,
+                              child: SfBarcodeGenerator(
+                                textStyle: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height /
+                                            50),
+                                value:
+                                    "${widget.currenttoken} ${now.toString()}",
+                                showValue: true,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
                   child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      WidgetsToImage(
-                        controller: controller,
-                        child: Container(
-                          height: 300,
-                          width: 300,
-                          child: DottedBorder(
-                            color: Colors.black,
-                            strokeWidth: 1,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  widget.currenttoken,
-                                  style: TextStyle(fontSize: 100),
-                                ),
-                                Container(
-                                  height: 80,
-                                  child: SfBarcodeGenerator(
-                                    value:
-                                        "${widget.currenttoken} ${now.toString()}",
-                                    showValue: true,
-                                  ),
-                                ),
-                              ],
-                            ),
+                      //SizedBox(height: M  ediaQuery.of(context).size.height/5,),
+                      Padding(
+                        padding: EdgeInsets.all(
+                            MediaQuery.of(context).size.height / 40),
+                        child: Center(
+                          child: CupertinoActivityIndicator(
+                            radius: MediaQuery.of(context).size.width / 7,
                           ),
                         ),
                       ),
-                      //  SizedBox(
-                      //    height: 10,
-                      //  ),
-                      //  Text(
-                      //    "Select Printer Device to print",
-                      //    style: TextStyle(color: Color(0xffc01c7b)),
-                      //  ),
-                      // onTapped?SizedBox(
-                      //     height:100,child: Center(child: CircularProgressIndicator())): Padding(
-                      //    padding: const EdgeInsets.all(8.0),
-                      //    child: Container(
-                      //      decoration: BoxDecoration(
-                      //        borderRadius: BorderRadius.circular(10),
-                      //        border: Border.all(color: Color(0xffc01c7b)),
-                      //      ),
-                      //      height: 300,
-                      //      child: ListView.separated(
-                      //        itemBuilder: (context, index) {
-                      //          return ListTile(
-                      //            title: Text(scanResult![index].device.name),
-                      //            subtitle: Text(scanResult![index].device.id.id),
-                      //            onTap: () => {
-                      //              setState(()  {
-                      //           onTapped=true;
-                      //              }),
-                      //              scanResult![index].device.disconnect(),
-                      //              printWithDevice(scanResult![index].device)
-                      //            },
-                      //          );
-                      //        },
-                      //        separatorBuilder: (context, index) =>
-                      //            const Divider(),
-                      //        itemCount: scanResult?.length ?? 0,
-                      //      ),
-                      //    ),
-                      //  ),
+                      Padding(
+                        padding: EdgeInsets.all(
+                            MediaQuery.of(context).size.height / 40),
+                        child: Text(
+                          "அச்சிடுதல் தயவு செய்து காத்திருக்கவும்",
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.height / 30,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(
+                            MediaQuery.of(context).size.height / 40),
+                        child: Center(
+                            child: Text(
+                          "Printing please wait",
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.height / 30,
+                          ),
+                          textAlign: TextAlign.center,
+                        )),
+                      ),
+
+                      //SizedBox(height: MediaQuery.of(context).size.height/5,),
                     ],
                   ),
                 ),
-              ),
+              ],
             ),
-
-            // Center(
-            //   child: ElevatedButton(
-            //       onPressed: () async {
-            //         await
-            //           widget.browser.open(
-            //             url: Uri.parse("http://signalr.timesmed.com/Login/KaveriLogin"),
-            //             );
-            //       },
-            //       child: Text("Open Chrome Safari Browser")),
-            // )
-          ],
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: Column(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     crossAxisAlignment: CrossAxisAlignment.center,
+            //     children: [
+            //       WidgetsToImage(
+            //         controller: controller,
+            //         child: Container(
+            //           height: 300,
+            //           width: 300,
+            //           child: DottedBorder(
+            //             color: Colors.black,
+            //             strokeWidth: 1,
+            //             child: Column(
+            //               crossAxisAlignment: CrossAxisAlignment.center,
+            //               mainAxisAlignment: MainAxisAlignment.center,
+            //               children: [
+            //                 Text(
+            //                   widget.currenttoken,
+            //                   style: TextStyle(fontSize: 100),
+            //                 ),
+            //                 Container(
+            //                   height: 80,
+            //                   child: SfBarcodeGenerator(
+            //                     value:
+            //                         "${widget.currenttoken} ${now.toString()}",
+            //                     showValue: true,
+            //                   ),
+            //                 ),
+            //               ],
+            //             ),
+            //           ),
+            //         ),
+            //       ),
+            //       //  SizedBox(
+            //       //    height: 10,
+            //       //  ),
+            //       //  Text(
+            //       //    "Select Printer Device to print",
+            //       //    style: TextStyle(color: Color(0xffc01c7b)),
+            //       //  ),
+            //       // onTapped?SizedBox(
+            //       //     height:100,child: Center(child: CircularProgressIndicator())): Padding(
+            //       //    padding: const EdgeInsets.all(8.0),
+            //       //    child: Container(
+            //       //      decoration: BoxDecoration(
+            //       //        borderRadius: BorderRadius.circular(10),
+            //       //        border: Border.all(color: Color(0xffc01c7b)),
+            //       //      ),
+            //       //      height: 300,
+            //       //      child: ListView.separated(
+            //       //        itemBuilder: (context, index) {
+            //       //          return ListTile(
+            //       //            title: Text(scanResult![index].device.name),
+            //       //            subtitle: Text(scanResult![index].device.id.id),
+            //       //            onTap: () => {
+            //       //              setState(()  {
+            //       //           onTapped=true;
+            //       //              }),
+            //       //              scanResult![index].device.disconnect(),
+            //       //              printWithDevice(scanResult![index].device)
+            //       //            },
+            //       //          );
+            //       //        },
+            //       //        separatorBuilder: (context, index) =>
+            //       //            const Divider(),
+            //       //        itemCount: scanResult?.length ?? 0,
+            //       //      ),
+            //       //    ),
+            //       //  ),
+            //     ],
+            //   ),
+            // ),
+          ),
         ),
       ),
     );
